474c4b2a960dd6d5c66218fdf35ee0fa9c5de52a
Christian Alexander Schreiber <91784401+christian-schreiber@users.noreply.github.com>
2023-11-11 16:51:05 +0100

    Update Forms.js

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -1,50 +1,31 @@
-import React from "react";
-import "./Form.css"
+function sendForm(event) {
+  event.preventDefault(); // Prevent the form from submitting in the traditional way
 
-function Forms() {
-    function sendForm() {
-        const firstnameInput = document.getElementById("firstname");
-        const lastnameInput = document.getElementById("lastname");
-        const emailInput = document.getElementById("email");
+  const firstnameInput = document.getElementById("firstname");
+  const lastnameInput = document.getElementById("lastname");
+  const emailInput = document.getElementById("email");
 
-        const firstname = firstnameInput.value;
-        const lastname = lastnameInput.value;
-        const email = emailInput.value;
+  const firstname = firstnameInput.value;
+  const lastname = lastnameInput.value;
+  const email = emailInput.value;
 
-        fetch("https://azuresaturdayapi.azure-api.net/upload", {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Ocp-Apim-Subscription-Key': '2c385dec383e419da10384e942d320b2',
-              },
-            body: {
-                "name": lastname,
-                "firstname": firstname,
-                "email": email
-            },          
-          })
-            .then((response) => response.json())
-            .then((data) => {
-              console.log('Response from server:', data);
-            })
-            .catch((error) => {
-              console.error('Error sending form data:', error);
-            });
-    }
-
-    return(
-        <>
-            <div className="formContainer">
-                <form className="userForm">
-                    <input type="text" inputMode="text" id="lastname" placeholder="Name*" required></input>
-                    <input type="text" inputMode="text" id="firstname" placeholder="Vorname*" required></input>
-                    <input type="text" inputMode="email" id="email" placeholder="Email*" required></input>
-                
-                    <button type="submit" onSubmit={sendForm}>Absenden</button>
-                </form>
-            </div>
-        </>
-    );
+  fetch("https://azuresaturdayapi.azure-api.net/upload", {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Ocp-Apim-Subscription-Key': '2c385dec383e419da10384e942d320b2',
+    },
+    body: JSON.stringify({
+      "name": lastname,
+      "firstname": firstname,
+      "email": email
+    }),          
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log('Response from server:', data);
+    })
+    .catch((error) => {
+      console.error('Error sending form data:', error);
+    });
 }
-
-export default Forms;
